d49313fd5ace94312486cf44ac69c1d45ddbe697
PawanSinghChaudhary-26 <pawansingh1894509@gmail.com>
2022-07-13 14:23:50 +0530

    prime code updated 13-07-2022

--- Week 1 Basics/prime.cpp
+++ Week 1 Basics/prime.cpp
@@ -1,21 +1,29 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
 int main() {
     int n;
     cin>>n;
-    for(int i=2;i<=n;)
+    if(n==0)
+    {
+        cout<<"Invalid Input \n";
+        exit(1);
+    }
+    bool flag=0;
+    for(int i=2;i<n/2; i++)
     {
         if(n%i==0)
         {
+            
         cout<<"not a prime number";
+        flag=1;
         break;
         }
-        else
-        cout<<"prime number";
-        break;
-        i++; 
+        
     }
+        if(flag==0)
+        {
+        cout<<"prime number";
+        }
     return 0;
 }
